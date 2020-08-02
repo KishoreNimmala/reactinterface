38a1383f22b4791355c8451ea41b5892ee271c17
Kishore Nimmala <nkishore779@gmail.com>
2020-08-02 11:25:05 +0530

    Using third-party components

--- src/components/ListAppointments.js
+++ src/components/ListAppointments.js
@@ -1,4 +1,6 @@
 import React,{component, Component} from 'react';
+import {MdPets} from 'react-icons/md';
+import Moment from 'react-moment';
 
 class ListAppointments extends Component{
     render(){
@@ -7,18 +9,25 @@ class ListAppointments extends Component{
             {this.props.appointments.map((item,i)=>(
                 <div key={item.aptId} className="pet-item col media py-3">
                     <div className="mr-3">
-                    <button className="pet-delete btn btn-sm btn-danger">X</button>
+                    <button className="pet-delete btn btn-sm btn-danger">
+                        <MdPets />
+                    </button>
                     </div>
         
                     <div className="pet-info media-body">
                     <div className="pet-head d-flex">
                         <span className="pet-name">{item.petName}</span>
-                        <span className="apt-date ml-auto">{item.aptDate}</span>
+                        <span className="apt-date ml-auto">
+                            <Moment 
+                                date={item.aptDate}
+                                parse="YYYY-MM-dd hh:mm"
+                                format="MMM-D h:mma"
+                            />
+                        </span>
                     </div>
         
                     <div className="owner-name">
-                        <span className="label-item">Owner: </span>
-                        <span>{item.ownerName}</span>
+                        <span className="label-item">Owner: {item.ownerName}</span>
                     </div>
                     <div className="apt-notes">{item.aptNotes}</div>
                     </div>
